2b3851d41d3ba5a07d92c728fc78ee3517abcae6
Tirth Patel <tbhanderi872@rku.ac.in>
2025-12-22 20:45:07 +0530

    created new folder for backend and initialized new spring boot project

---
+++ backend/AITestGenerator/src/main/java/com/AITestGenerator/AITestGenerator/AiTestGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.AITestGenerator.AITestGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTestGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiTestGeneratorApplication.class, args);
+	}
+
+}
---
+++ backend/AITestGenerator/src/test/java/com/AITestGenerator/AITestGenerator/AiTestGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AITestGenerator.AITestGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiTestGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
